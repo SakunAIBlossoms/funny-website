a99264df177fba5a1ca47827bf385dc01d145cc1
SakunAI <110323819+SakunAIBlossoms@users.noreply.github.com>
2025-03-04 11:10:47 +1030

    tons and tons of updates i made to the entire ting in general lol

--- halloffame.js
+++
@@ -1,6 +0,0 @@
-let body = document.querySelector('body');
-body.addEventListener('click', function(event){
-    let spark = document.createElement('div');
-    spark.classList.add('spark');
-    body.appendChild(spark);
-})
